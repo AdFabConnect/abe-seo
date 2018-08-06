775704daf8523b010bc7ef956d7c72f95518f04d
wonknu10 <fabrice.labbe@adfab.fr>
2018-08-06 14:43:22 -0400

    fix for v2.13

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -4,7 +4,56 @@ var path = require('path');
 var fs = require('fs');
 
 function getHreflangs(json, abe) {
-	var hreflangs = []
+	// var hreflangs = null
+
+	// if (json.abe_meta != null && abe.config.seo != null && abe.config.seo.templates != null && abe.config.seo.templates[json.abe_meta.template] != null) {
+	// 	hreflangs = []
+	// 	var regex = abe.config.seo.templates[json.abe_meta.template].regex
+	// 	var substitute = abe.config.seo.templates[json.abe_meta.template].substitute
+	// 	var variable = abe.config.seo.templates[json.abe_meta.template].variable
+
+	// 	var files = abe.Manager.instance.getList()
+	// 	var pathToTest = json.abe_meta.link.replace(new RegExp(regex), substitute)
+	// 	pathToTest = new RegExp(pathToTest)
+
+	// 	Array.prototype.forEach.call(files, function(file) {
+	// 		if (file.publish) {
+	// 			if (pathToTest.test(file.abe_meta.link)) {
+	// 				var lang = ""
+	// 				try {
+	// 					var lang = eval("file." + variable)
+	// 				}catch(e) {
+	// 				}
+	// 				if(typeof lang === 'undefined' || lang === null || lang === "") {
+	// 					var jsonFile = JSON.parse(fs.readFileSync(file.publish.path, 'utf8'));
+	// 					try {
+	// 						var lang = eval("jsonFile." + variable)
+	// 					}catch(e) {
+
+	// 					}
+	// 				}
+
+	// 				if(abe.config && abe.config.seo && abe.config.seo.replace && abe.config.seo.replace[lang]) {
+	// 					lang = abe.config.seo.replace[lang]
+	// 				}
+
+	// 				var filePath = (abe.config && abe.config.seo && abe.config.seo.domain)
+	// 					? abe.config.seo.domain.replace(/\/$/, '') + '/' + file.abe_meta.link.replace(/^\//, '')
+	// 					: checkFile.filePath
+
+	// 				var hreflang = {
+	// 					hreflang: lang,
+	// 					url: filePath,
+	// 					html: file.abe_meta.link
+	// 				}
+	// 				hreflangs.push(hreflang)
+	// 			}
+	// 		}
+	// 	})
+	// }
+
+  // return hreflangs
+  var hreflangs = []
 	var regLang = new RegExp('^\/([a-zA-z-]*?)\/')
 	if(abe.config && abe.config.seo && abe.config.seo.regex) {
 		regLang = new RegExp(abe.config.seo.regex)
@@ -59,91 +108,92 @@ function getHreflangs(json, abe) {
 			}
 		})
 	}
-
 	return hreflangs
 }
 
 var hooks = {
-  afterEditorInput: function afterEditorInput(htmlString, params, abe) {
-  	if(typeof params.value !== 'undefined' && params.value !== null && params.value !== '' && params.key === 'abe_hreflangs') {
-  		var hreflangs = getHreflangs(params.value, abe)
-			htmlString =  '<div class="form-group">'
-      htmlString += '  <label for="href_langs">'
-      htmlString += '    Href langs'
-      htmlString += '  </label>'
-			htmlString += '  <div class="tags-wrapper">';
-			htmlString += '  <ul>';
-			Array.prototype.forEach.call(hreflangs, function(hreflang) {
-					htmlString  += '  <li>' + hreflang.url + ' (' + hreflang.hreflang + ')</li>';
-			})
-			htmlString += '  </ul>';
-			htmlString += '  </div>';
-      htmlString += '</div>'
-    }
+	afterEditorInput: function afterEditorInput(htmlString, params, abe) {
+		if(typeof params.value !== 'undefined' && params.value !== null && params.value !== '' && params.key === 'abe_hreflangs') {
+        var hreflangs = getHreflangs(params.value, abe)
+	  		if (hreflangs != null) {
+				htmlString =  '<div class="form-group">'
+				htmlString += '  <label for="href_langs">'
+				htmlString += '    Href langs'
+				htmlString += '  </label>'
+				htmlString += '  <div class="tags-wrapper">';
+				htmlString += '  <ul>';
+				Array.prototype.forEach.call(hreflangs, function(hreflang) {
+					htmlString  += '  <li><a href="/abe/editor' + hreflang.html + '">' + hreflang.url + ' (' + hreflang.hreflang + ')</a></li>';
+				})
+				htmlString += '  </ul>';
+				htmlString += '  </div>';
+				htmlString += '</div>'
+	  		}
+	    }
 
 		return htmlString;
 	},
-	afterEditorFormBlocks: function afterEditorFormBlocks(blocks, json, abe) {
+	afterEditorFormBlocks: function afterEditorFormBlocks(blocks, json, text, abe) {
     blocks['Seo'] = {
-    	input_tags:[
-				{
-					type: 'text',
-					key: 'abe_hreflangs',
-					desc: 'hreflangs',
-					maxLength: '',
-					tab: 'Seo',
-					placeholder: '',
-					value: json,
-					source: null,
-					display: '',
-					reload: false,
-					order: '99999',
-					required: '',
-					editable: false,
-					visible: '',
-					block: '',
-					autocomplete: ''
+    	input_tags:[{
+        type: 'text',
+        key: 'abe_hreflangs',
+        desc: 'hreflangs',
+        maxLength: '',
+        tab: 'Seo',
+        placeholder: '',
+        value: json,
+        source: null,
+        display: '',
+        reload: false,
+        order: '99999',
+        required: '',
+        editable: false,
+        visible: '',
+        block: '',
+        autocomplete: ''
+      }]
+    };
+		return blocks;
+	},
+	beforePublish: function(json, filepath, abe) {
+    var hreflangs = getHreflangs(json, abe);
+		if(typeof hreflangs !== 'undefined' && hreflangs !== null) {
+			try {
+				var lang = eval("json." + abe.config.seo.variable)
+				var filePath = (abe.config && abe.config.seo && abe.config.seo.domain)
+						? abe.config.seo.domain.replace(/\/$/, '') + '/' + json.abe_meta.link.replace(/^\//, '')
+						: json.abe_meta.link
+
+				var hasCanonical = true;
+				var configCanonical = abe.config.seo.excludeCanonicalTemplates;
+				json.seoPlugin = {};
+				if((typeof configCanonical === 'undefined' || configCanonical === null) || configCanonical.indexOf(json.abe_meta.template) < 0) {
+					json.seoPlugin['canonical'] = {
+						url: filePath,
+						hreflang: lang
+					};
 				}
-			]
-    }
+					
+        json.seoPlugin['hreflangs'] = getHreflangs(json, abe);
+			}catch(e) {
 
-    return blocks;
-  },
-	beforeSave: function(obj, abe) {
-		if (obj.type === 'publish') {
-			var hreflangs = getHreflangs(obj.json.content, abe)
-			obj.json.content['seoPlugin'] = hreflangs;
-
-			if (!obj.publishAll) {
-				var htmlHreflangs = ''
-				Array.prototype.forEach.call(hreflangs, function(hreflang) {
-					htmlHreflangs += '<link rel=\"alternate\" href=\"' + hreflang.url + '\" hreflang=\"' + hreflang.hreflang + '\" />\n'
-				})
-				htmlHreflangs += '</head>'
-
-				Array.prototype.forEach.call(hreflangs, function(hreflang) {
-					var htmlPath = path.join(abe.config.root, abe.config.publish.url, hreflang.html)
-					try{
-						var html = fs.readFileSync(htmlPath, 'utf8')
-						html = html.replace(/<link rel="alternate".+\n/g, '')
-						html = html.replace(/<\/head>/, htmlHreflangs)
-						abe.fse.writeFileSync(htmlPath, html, {encoding: 'utf8'})
-					}
-					catch(e){}
-				})
 			}
 		}
 
-		return obj
+		return json
 	},
 	afterGetTemplate: function(text, abe) {
-		var str = "\n{{#each seoPlugin}}\n"
-				str += "<link rel=\"alternate\" href=\"{{url}}\" hreflang=\"{{hreflang}}\" />\n"
-				str += "{{/each}}\n"
-				str += '</head>'
+		var str = "\n{{#if seoPlugin}}\n"
+			str += "    {{#if seoPlugin.canonical}}<link rel=\"canonical\" href=\"{{seoPlugin.canonical.url}}\" />\n{{/if}}"
+			str += "{{#each seoPlugin.hreflangs}}\n"
+			str += "    <link rel=\"alternate\" href=\"{{url}}\" hreflang=\"{{hreflang}}\" />\n"
+			str += "{{/each}}\n"
+			str += '</head>'
+			str += '{{/if}}'
 		text = text.replace(/<\/head>/, str)
-    return text
-  }
+		return text
+	}
 };
 
 exports.default = hooks;
